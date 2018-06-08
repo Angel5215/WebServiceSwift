04997a4917e3414e98aaf08ca30aaa95dc3f8023
CobainJosue <chivasagrada@live.com.mx>
2018-06-08 13:18:00 -0500

    Consulta de topico

--- Sources/proyecto/main.swift
+++ Sources/proyecto/main.swift
@@ -6,8 +6,15 @@ import MongoKitten
 import ExtendedJSON
 
 //	Database configuration (2 meses)
-let myDatabase = try MongoKitten.Database("mongodb://192.168.0.15/espaciales")
-let salones = myDatabase["salon"] // colección en MongoDB
+let myDatabase = try MongoKitten.Database("mongodb://localhost/espaciales")
+//conexion para cada coleccion de la base de datos
+let estacionamiento = myDatabase["estacionamiento"] // colección en MongoDB
+let actividad = myDatabase["actividad"]
+let comida = myDatabase["comida"]
+let facultad = myDatabase["facultad"]
+let representativo = myDatabase["representativo"]
+let ruta = myDatabase["ruta"]
+
 
 //	Bitácora (warnings, errores, info, verbose, etc...)
 HeliumLogger.use()
@@ -21,17 +28,40 @@ router.setDefault(templateEngine: StencilTemplateEngine())
 
 //	router.get(path: String, handler: (Request, Response, Next) -> ())
 //	Trailing Closure
-router.get("/") {
+router.get("/:topico") {
 	request, response, next in 
 	
 	//	defer se llama al final de terminar el método
 	defer { next() }
 
 	//	select * from espaciales.salon as salones; [Document] coordinates: [1, 2] BSON
-	for salon in try salones.find() {
+/*	for salon in try salones.find() {
 		response.send(salon.makeExtendedJSON().serializedString())
+	}*/
+
+	guard let topico = request.parameters["topico"] else {
+		try response.status(.badRequest).end()
+		return
 	}
+
+	//select * from topico
+	if topico == "estacionamiento" {
+
+		let query = try Array(estacionamiento.find())
+
+		print(query)
+		print(query.count)
+		response.send(query.makeExtendedJSON().serializedString())
+		//response.send("\n")
+
+
+	}
+
+	
+
 }
+
+
 
 
 Kitura.addHTTPServer(onPort: 8090, with: router)
