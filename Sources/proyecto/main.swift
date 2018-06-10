a4a8c6aea3bfbdb510d9ee71a17a14ce8739cc7a
CobainJosue <chivasagrada@live.com.mx>
2018-06-10 13:37:46 -0500

    Se insertan registros por topico en la base de MongoDB

--- Sources/proyecto/main.swift
+++ Sources/proyecto/main.swift
@@ -50,6 +50,49 @@ router.get("/:topico") {
 
 }
 
+//Inserción de un punto definido
+router.post("/insert/:topico", middleware: BodyParser())
+router.post("/insert/:topico") {
+	request, response, next in
+
+	//	defer se llama al final de terminar el método
+	defer { next() }
+
+	guard let topico = request.parameters["topico"] else {
+		try response.status(.badRequest).end()
+		return
+	}
+
+	let coleccion = myDatabase[topico]
+
+	guard let values = request.body else {
+		try response.status(.badRequest).end()
+		return
+	}
+
+	guard case .urlEncoded(let body) = values else {
+		try response.status(.badRequest).end()
+		return
+	}
+
+	if let latitudCadena = body["latitud"], let longitudCadena = body["longitud"], let latitud = Double(latitudCadena), let longitud = Double(longitudCadena), let titulo = body["titulo"], let descripcion = body["descripcion"] {
+
+
+		//let position = try Position(values: [longitud, latitud])
+		
+		let nuevo: Document = [ "type": "Feature", 
+		"properties": [ "Name": titulo, "description": descripcion, "tessellate": -1, "extrude": 0, "visibility": -1 ], 
+		"geometry": [ "type": "Point", "coordinates": [longitud, latitud, 0 ]]]
+
+		try coleccion.insert(nuevo)
+
+		response.send(nuevo.makeExtendedJSON().serializedString())
+
+	}
+
+
+}
+
 //Buffer para buscar topicos a partir de la ubicacion actual
 router.post("/buffer/:topico", middleware: BodyParser())
 router.post("/buffer/:topico"){
